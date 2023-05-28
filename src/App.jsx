62bdf6101c75bb61104f73e14b4269480031a55d
thuanphatt <thphatt@gmail.com>
2023-05-28 21:45:41 +0700

    set up stucture folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Space } from "antd";
+import Header from "./components/Header";
+import SideMenu from "./components/SideMenu";
+import PageContent from "./components/PageContent";
+import Footer from "./components/Footer";
 
 function App() {
-  return <>Hello World!!!</>;
+  return (
+    <div className="app">
+      <Header />
+      <Space>
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </Space>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
