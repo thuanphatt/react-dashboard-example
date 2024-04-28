1338f956670546c0c6978228e5de296fe44ed007
Nguyễn Thuận Phát <88497622+thuanphatt@users.noreply.github.com>
2024-04-28 22:46:39 +0700

    learnt about react-reselect, react formik #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useFormik } from "formik";
 import "./App.css";
-import _ from "lodash";
 
 function App() {
-	const a = [1, 2];
-
-	console.log(_.concat(a, [2, 3, 4]));
-	return <div className="app">hello</div>;
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			username: "",
+		},
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+	return (
+		<form onSubmit={formik.handleSubmit}>
+			<label htmlFor="email">Email Address</label>
+			<input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
+			<input id="username" name="username" type="text" onChange={formik.handleChange} value={formik.values.username} />
+			<button type="submit">Submit</button>
+		</form>
+	);
 }
 
 export default App;
